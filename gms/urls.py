e81e8bb1c7c45256cd846b1e0507494a82d505ab
Shubham <byshubham6@gmail.com>
2024-05-28 09:56:50 +0530

    #

---
+++ gms/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home, name='home'),
+    path('about/', views.About, name='about'),
+    path('contact/', views.Contact, name='contact'),
+    path('admin_login/', views.Login, name='login'),
+    path('logout/', views.Logout, name='logout'),
+
+    path('add_enquiry/', views.Add_Enquiry, name='add_enquiry'),
+    path('view_enquiry/', views.View_Enquiry, name='view_enquiry'),
+    path('delete_enquiry/<int:pid>/', views.Delete_Enquiry, name='delete_enquiry'),
+
+    path('add_equipment/', views.Add_Equipment, name='add_equipment'),
+    path('view_equipment/', views.View_Equipment, name='view_equipment'),
+    path('delete_equipment/<int:pid>/', views.Delete_Equipment, name='delete_equipment'),
+
+    path('add_plan/', views.Add_Plan, name='add_plan'),
+    path('view_plan/', views.View_Plan, name='view_plan'),
+    path('delete_plan/<int:pid>/', views.Delete_Plan, name='delete_plan'),
+
+    path('add_member/', views.Add_Member, name='add_member'),
+    path('view_member/', views.View_Member, name='view_member'),
+    path('delete_member/<int:pid>/', views.Delete_Member, name='delete_member'),
+]
